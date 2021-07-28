f42c14f5ec8dd7d5a21ec2338c3e6605ae887cbf
Alexis Quintuna <alexisquintuna@Alexiss-MacBook-Air.local>
2021-07-28 13:25:39 -0400

    initial commit

---
+++ lookup/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#anytime we add a new webpage it goes in here
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+]
+
